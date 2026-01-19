58e2e82c1c809718f9c3d72342c88dbfb3c78802
TruongVN <vungoctruong8x@gmail.com>
2026-01-19 23:49:38 +0700

    Code base for timer PWM mode

---
+++ Core/Inc/tim.h
@@ -0,0 +1,83 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    tim.h
+  * @brief   This file contains all the function prototypes for
+  *          the tim.c file
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __TIM_H__
+#define __TIM_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* USER CODE BEGIN Private defines */
+#define MOTOR_PWM1 LL_TIM_CHANNEL_CH1
+#define MOTOR_PWM2 LL_TIM_CHANNEL_CH2
+
+// Servo Identifiers
+typedef enum {
+    SERVO1 = 0,
+    SERVO2 = 1,
+    SERVO_MAX_ID
+} Servo_ID;
+
+// Struct for servo configuration
+typedef struct {
+    Servo_ID id;
+    uint32_t timer_channel; // Timer channel used for PWM
+    int16_t min_angle;      // Minimum angle (usually 0 degrees)
+    int16_t max_angle;      // Maximum angle (usually 180 degrees)
+    uint32_t min_pulse;     // Minimum pulse width in microseconds (e.g., 500us)
+    uint32_t max_pulse;     // Maximum pulse width in microseconds (e.g., 2500us)
+} Servo_Config;
+
+/* USER CODE END Private defines */
+
+void MX_TIM2_Init(void);
+void MX_TIM3_Init(void);
+void MX_TIM4_Init(void);
+
+/* USER CODE BEGIN Prototypes */
+
+/* TIM4 PWM Control Functions  */
+void TIM4_enable_PWM(uint32_t Channel);
+void TIM4_disable_PWM(uint32_t Channel);
+void TIM4_Set_PWM_DutyCycle(uint32_t Channel, uint32_t DutyCycle);
+
+/* Servo Control Functions */
+// Control servo motor (0-180 degrees)
+// PWM frequency: 50Hz
+// Pulse width: 0.5ms (0 deg) to 2.5 ms (180 deg)
+void Servo_Init(void);
+void Servo_setAngle(Servo_ID servo_id, int16_t angle);
+
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __TIM_H__ */
+
---
+++ Core/Src/tim.c
@@ -0,0 +1,278 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    tim.c
+  * @brief   This file provides code for the configuration
+  *          of the TIM instances.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "tim.h"
+
+/* USER CODE BEGIN 0 */
+
+// CLock for TIM2-TMI7: 84 MHz (APB1 x2)
+// Clock for TIM1, TIM8: 168 MHz (APB2 x2)
+
+// Define servo configurations
+Servo_Config servo1_config = {
+    .id = SERVO1,
+    .timer_channel = LL_TIM_CHANNEL_CH1,
+    .min_angle = 0,
+    .max_angle = 180,
+    .min_pulse = 500,   // 0.5 ms
+    .max_pulse = 2500   // 2.5 ms
+};
+
+Servo_Config servo2_config = {
+    .id = SERVO2,
+    .timer_channel = LL_TIM_CHANNEL_CH2,
+    .min_angle = 0,
+    .max_angle = 180,
+    .min_pulse = 500,   // 0.5 ms
+    .max_pulse = 2500   // 2.5 ms
+};
+
+// Array of servo configurations
+Servo_Config* servo_configs[SERVO_MAX_ID] = {
+    &servo1_config,
+    &servo2_config
+};
+
+/* USER CODE END 0 */
+
+/* TIM2 init function */
+void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  LL_TIM_InitTypeDef TIM_InitStruct = {0};
+
+  /* Peripheral clock enable */
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
+
+  /* TIM2 interrupt Init */
+  NVIC_SetPriority(TIM2_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),0, 0));
+  NVIC_EnableIRQ(TIM2_IRQn);
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  TIM_InitStruct.Prescaler = 83;
+  TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
+  TIM_InitStruct.Autoreload = 499999;
+  TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
+  LL_TIM_Init(TIM2, &TIM_InitStruct);
+  LL_TIM_DisableARRPreload(TIM2);
+  LL_TIM_SetClockSource(TIM2, LL_TIM_CLOCKSOURCE_INTERNAL);
+  LL_TIM_SetTriggerOutput(TIM2, LL_TIM_TRGO_RESET);
+  LL_TIM_DisableMasterSlaveMode(TIM2);
+  /* USER CODE BEGIN TIM2_Init 2 */
+  // Enable update interrupt
+  LL_TIM_EnableIT_UPDATE(TIM2);
+  // Start TIM2 counter
+    LL_TIM_EnableCounter(TIM2);
+
+  /* USER CODE END TIM2_Init 2 */
+
+}
+/* TIM3 init function */
+void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  LL_TIM_InitTypeDef TIM_InitStruct = {0};
+
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* Peripheral clock enable */
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM3);
+
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+  /**TIM3 GPIO Configuration
+  PA6   ------> TIM3_CH1
+  */
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_6;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_2;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  TIM_InitStruct.Prescaler = 83;
+  TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
+  TIM_InitStruct.Autoreload = 65535;
+  TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
+  LL_TIM_Init(TIM3, &TIM_InitStruct);
+  LL_TIM_DisableARRPreload(TIM3);
+  LL_TIM_SetClockSource(TIM3, LL_TIM_CLOCKSOURCE_INTERNAL);
+  LL_TIM_SetTriggerOutput(TIM3, LL_TIM_TRGO_RESET);
+  LL_TIM_DisableMasterSlaveMode(TIM3);
+  LL_TIM_IC_SetActiveInput(TIM3, LL_TIM_CHANNEL_CH1, LL_TIM_ACTIVEINPUT_DIRECTTI);
+  LL_TIM_IC_SetPrescaler(TIM3, LL_TIM_CHANNEL_CH1, LL_TIM_ICPSC_DIV1);
+  LL_TIM_IC_SetFilter(TIM3, LL_TIM_CHANNEL_CH1, LL_TIM_IC_FILTER_FDIV1);
+  LL_TIM_IC_SetPolarity(TIM3, LL_TIM_CHANNEL_CH1, LL_TIM_IC_POLARITY_RISING);
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+
+}
+/* TIM4 init function */
+void MX_TIM4_Init(void)
+{
+
+  /* USER CODE BEGIN TIM4_Init 0 */
+
+  /* USER CODE END TIM4_Init 0 */
+
+  LL_TIM_InitTypeDef TIM_InitStruct = {0};
+  LL_TIM_OC_InitTypeDef TIM_OC_InitStruct = {0};
+
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* Peripheral clock enable */
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM4);
+
+  /* USER CODE BEGIN TIM4_Init 1 */
+
+  /* USER CODE END TIM4_Init 1 */
+  TIM_InitStruct.Prescaler = 83;
+  TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
+  TIM_InitStruct.Autoreload = 19999;
+  TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
+  LL_TIM_Init(TIM4, &TIM_InitStruct);
+  LL_TIM_EnableARRPreload(TIM4);
+  LL_TIM_SetClockSource(TIM4, LL_TIM_CLOCKSOURCE_INTERNAL);
+  LL_TIM_OC_EnablePreload(TIM4, LL_TIM_CHANNEL_CH1);
+  TIM_OC_InitStruct.OCMode = LL_TIM_OCMODE_PWM1;
+  TIM_OC_InitStruct.OCState = LL_TIM_OCSTATE_DISABLE;
+  TIM_OC_InitStruct.OCNState = LL_TIM_OCSTATE_DISABLE;
+  TIM_OC_InitStruct.CompareValue = 0;
+  TIM_OC_InitStruct.OCPolarity = LL_TIM_OCPOLARITY_HIGH;
+  LL_TIM_OC_Init(TIM4, LL_TIM_CHANNEL_CH1, &TIM_OC_InitStruct);
+  LL_TIM_OC_DisableFast(TIM4, LL_TIM_CHANNEL_CH1);
+  LL_TIM_OC_EnablePreload(TIM4, LL_TIM_CHANNEL_CH2);
+  TIM_OC_InitStruct.CompareValue = 500;
+  LL_TIM_OC_Init(TIM4, LL_TIM_CHANNEL_CH2, &TIM_OC_InitStruct);
+  LL_TIM_OC_DisableFast(TIM4, LL_TIM_CHANNEL_CH2);
+  TIM_OC_InitStruct.OCMode = LL_TIM_OCMODE_TOGGLE;
+  TIM_OC_InitStruct.CompareValue = 300;
+  LL_TIM_OC_Init(TIM4, LL_TIM_CHANNEL_CH3, &TIM_OC_InitStruct);
+  LL_TIM_OC_DisableFast(TIM4, LL_TIM_CHANNEL_CH3);
+  LL_TIM_SetTriggerOutput(TIM4, LL_TIM_TRGO_RESET);
+  LL_TIM_DisableMasterSlaveMode(TIM4);
+  LL_TIM_OC_EnablePreload(TIM4, LL_TIM_CHANNEL_CH3);
+  /* USER CODE BEGIN TIM4_Init 2 */
+    // Start TIM4 channel 1 in PWM mode
+    LL_TIM_CC_EnableChannel(TIM4, LL_TIM_CHANNEL_CH3);
+    LL_TIM_EnableCounter(TIM4);
+
+  /* USER CODE END TIM4_Init 2 */
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOD);
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+    /**TIM4 GPIO Configuration
+    PD12     ------> TIM4_CH1
+    PB7     ------> TIM4_CH2
+    PB8     ------> TIM4_CH3
+    */
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_12;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_2;
+  LL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_7|LL_GPIO_PIN_8;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_2;
+  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 1 */
+
+void TIM4_enable_PWM(uint32_t Channel) {
+    LL_TIM_CC_EnableChannel(TIM4, Channel);
+}
+
+void TIM4_disable_PWM(uint32_t Channel) {
+    LL_TIM_CC_DisableChannel(TIM4, Channel);
+}
+
+// Timer4 set PWM duty cycle
+void TIM4_Set_PWM_DutyCycle(uint32_t Channel, uint32_t DutyCycle) {
+    // Assuming DutyCycle is in microseconds for servo control
+    // Convert microseconds to timer counts
+    // Timer clock = 1 MHz (1 count = 1 microsecond)
+    switch (Channel) {
+        case LL_TIM_CHANNEL_CH1:
+            LL_TIM_OC_SetCompareCH1(TIM4, DutyCycle);
+            break;
+        case LL_TIM_CHANNEL_CH2:
+            LL_TIM_OC_SetCompareCH2(TIM4, DutyCycle);
+            break;
+        case LL_TIM_CHANNEL_CH3:
+            break;
+        default:
+            return; // Invalid channel
+    }
+
+}
+
+// Config servo motor
+void Servo_Init(void) {
+    // Enable PWM on the specified channel
+    for (int i = 0; i < SERVO_MAX_ID; i++) {
+        Servo_Config* config = servo_configs[i];
+        TIM4_enable_PWM(config->timer_channel);
+    }
+}
+
+void Servo_setAngle(Servo_ID servo_id, int16_t angle) {
+    if (servo_id >= SERVO_MAX_ID) {
+        return; // Invalid servo ID
+    }
+
+    Servo_Config* config = servo_configs[servo_id];
+
+    // Clamp angle to min and max
+    if (angle < config->min_angle) {
+        angle = config->min_angle;
+    } else if (angle > config->max_angle) {
+        angle = config->max_angle;
+    }
+
+    // Map angle to pulse width
+    uint32_t pulse_width = config->min_pulse +
+        ((config->max_pulse - config->min_pulse) * (angle - config->min_angle)) /
+        (config->max_angle - config->min_angle);
+
+    // Set PWM duty cycle (pulse width in microseconds)
+    TIM4_Set_PWM_DutyCycle(config->timer_channel, pulse_width);
+}
+
+/* USER CODE END 1 */
